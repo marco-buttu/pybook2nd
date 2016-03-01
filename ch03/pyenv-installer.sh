1b5ece3121c9c6cf14272f5dd35bb2c95cbdebc5
Marco Buttu <marco.buttu@gmail.com>
2016-03-01 16:13:19 +0100

    Added ch03/pyenv-installer.sh

---
+++ ch03/pyenv-installer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+BASE_PATH=https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin
+
+if hash wget 2>/dev/null; then
+    wget --no-check-certificate -qO- $BASE_PATH/pyenv-installer | bash
+else
+    if hash curl 2>/dev/null; then
+        curl -L $BASE_PATH/pyenv-installer | bash
+    else
+        echo 'Both curl and wget are not installed, can not continue.'
+    fi
+fi
