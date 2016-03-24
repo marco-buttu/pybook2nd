e0641787fc06498d79697af84048aa90c5a945ce
Marco Buttu <marco.buttu@gmail.com>
2016-03-24 10:58:52 +0100

    Added a solution (about exception) to ch02

--- ch02/practice/digit2word.py
+++ ch02/practice/digit2word.py
@@ -1,4 +1,17 @@
 def digit2word(digit):
+    d = {0: 'zero', 1: 'uno', 2: 'due', 3: 'tre', 4: 'quattro'}
+    try:
+        return d[digit]
+    except KeyError:
+        return 'unknown'
+
+
+def digit2word_2nd(digit):
+    d = {0: 'zero', 1: 'uno', 2: 'due', 3: 'tre', 4: 'quattro'}
+    return d[digit] if digit in d else 'unknown'
+
+
+def digit2word_3rd(digit):
     if digit == 0:
         return 'zero'
     elif digit == 1:
@@ -11,8 +24,3 @@ def digit2word(digit):
         return 'quattro'
     else:
         return 'unknown'
-
-
-def digit2word_2nd(digit):
-    d = {0: 'zero', 1: 'uno', 2: 'due', 3: 'tre', 4: 'quattro'}
-    return d[digit] if digit in d else 'unknown'
--- ch02/practice/tests02.py
+++ ch02/practice/tests02.py
@@ -1,5 +1,5 @@
 import unittest
-from digit2word import digit2word, digit2word_2nd
+from digit2word import digit2word, digit2word_2nd, digit2word_3rd
 
 
 mapping = {0: 'zero', 1: 'uno', 2: 'due', 3: 'tre', 4: 'quattro'}
@@ -11,11 +11,13 @@ class TestDigit2Word(unittest.TestCase):
         for key, value in mapping.items():
             self.assertEqual(digit2word(key), value)
             self.assertEqual(digit2word_2nd(key), value)
+            self.assertEqual(digit2word_3rd(key), value)
 
     def test_wrong_value(self):
         """Return 'unknown' in case of wrong value"""
         self.assertEqual(digit2word('pippo'), 'unknown')
         self.assertEqual(digit2word_2nd(5), 'unknown')
+        self.assertEqual(digit2word_3rd(5), 'unknown')
 
 
 if __name__ == '__main__':
